c2f7941d1f730452521a202d1d3dd6217e546b2c
Shuva Kharel <shuvakharel007@gmail.com>
2025-06-12 12:06:26 +0545

    Module 01, 02 & 03(Control Structures)

--- 01 - Baiscs/assets/scripts/app.js
+++ 01 - Baiscs/assets/scripts/app.js
@@ -1,4 +1,51 @@
-let current_result = 0
-current_result += 10
+const defaultResult = 0;
+let currentResult = defaultResult;
+const logEntries = [];
 
-outputResult(current_result, '10')
+function getUserInput() {
+  return parseFloat(userInput.value);
+}
+
+function writeToLog(previousResult, operation, enteredNumber, currentResult) {
+  const logEntry = {
+    previousResult: previousResult,
+    operation: operation,
+    enteredNumber: enteredNumber,
+    currentResult: currentResult
+  }
+  logEntries.push(logEntry)
+  console.log(logEntry)
+}
+
+function calculateResult(operation) {
+  let previousResult = currentResult;
+  let enteredNumber = getUserInput();
+
+  if ((operation == '+' ||
+    operation == '-' ||
+    operation == '*' ||
+    (operation == '/' && enteredNumber != 0)) &&
+    !isNaN(enteredNumber)) {
+    createAndWriteOutput(operation, previousResult, enteredNumber);
+  }
+}
+
+function createAndWriteOutput(operation, previousResult, enteredNumber) {
+  if (operation === '+') {
+    currentResult += enteredNumber;
+  } else if (operation === '-') {
+    currentResult -= enteredNumber;
+  } else if (operation === '*') {
+    currentResult *= enteredNumber;
+  } else if (operation === '/') {
+    currentResult /= enteredNumber;
+  }
+
+  outputResult(currentResult, `${previousResult} ${operation} ${enteredNumber}`);
+  writeToLog(previousResult, operation, enteredNumber, currentResult);
+}
+
+addBtn.addEventListener('click', () => calculateResult('+'));
+subtractBtn.addEventListener('click', () => calculateResult('-'));
+multiplyBtn.addEventListener('click', () => calculateResult('*'));
+divideBtn.addEventListener('click', () => calculateResult('/'));
